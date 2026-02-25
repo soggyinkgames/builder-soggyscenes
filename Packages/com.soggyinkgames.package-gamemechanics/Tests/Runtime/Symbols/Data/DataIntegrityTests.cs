54e8bdb1a1442b9871307bbeede69d8a66e69eb7
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2026-02-25 17:11:59 +1100

    test folders

---
+++ Packages/com.soggyinkgames.package-gamemechanics/Tests/Runtime/Symbols/Data/DataIntegrityTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using UnityEngine;
+using SoggyInkGames.Equanimous.PackageGameMechanics.Symbols;
+
+public class DataIntegrityTests
+{
+    [Test]
+    public void CanCreateSymbolDefinition()
+    {
+        var symbol = ScriptableObject.CreateInstance<SymbolDefinition>();
+        symbol.Id = "DATA";
+
+        Assert.AreEqual("DATA", symbol.Id);
+    }
+
+    [Test]
+    public void CanCreateGestureDefinition()
+    {
+        var gesture = ScriptableObject.CreateInstance<GestureDefinition>();
+        gesture.ReferencePath = TestPaths.Line();
+
+        Assert.NotNull(gesture.ReferencePath);
+    }
+}
