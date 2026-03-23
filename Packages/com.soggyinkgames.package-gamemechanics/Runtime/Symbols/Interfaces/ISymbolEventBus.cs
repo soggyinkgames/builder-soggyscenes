78b86a51b598dfa33f36c4f81ab398b737ea864c
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2026-03-24 09:44:50 +1100

    fix(YN-008): remove unused code

--- Packages/com.soggyinkgames.package-gamemechanics/Runtime/Symbols/Interfaces/ISymbolEventBus.cs
+++
@@ -1,7 +0,0 @@
-public interface ISymbolEventBus
-{
-    event Action<SymbolIdentity> SymbolRecognized;
-    event Action<string> SymbolFailed;
-    void RaiseRecognized(SymbolIdentity identity);
-    void RaiseFailed(string reason);
-}
